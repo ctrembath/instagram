09c9d85509bbd3c1838324539fee704d54534d26
John <jjkiely@gmail.com>
2015-01-12 15:06:37 +0000

    tests modified to accomidate user login

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'comments' do
 
 
+
   context 'when leaving a comment' do
     # before do
     #   Post.create(name:'First')
@@ -10,14 +11,38 @@ describe 'comments' do
 
     let!(:first){Post.create(name:'First')}
 
-    it 'lets users leave a comment' do
-      visit '/'
-      click_link 'First'
-      click_link 'Leave a Comment'
-      fill_in "Comment", with: 'This is a comment'
-      click_button 'Leave Comment'
-      expect(current_path).to eq "/posts/#{first.id}"
-      expect(page).to have_content 'This is a comment' 
+    context 'when signed out' do
+
+      it 'does not let users leave a comment' do
+        visit '/'
+        click_link 'First'
+        click_link 'Leave a Comment'
+        expect(page).to have_content 'Sign up'
+      end
+
+    end
+
+    context 'when signed in' do
+
+      before do
+        visit '/'
+        click_link('Register')
+        fill_in('Email', with: 'test@tester.com')
+        fill_in('Password', with: 'testtest')
+        fill_in('Password confirmation', with: 'testtest')
+        click_button('Sign up')
+      end
+
+      it 'lets users leave a comment' do
+        visit '/'
+        click_link 'First'
+        click_link 'Leave a Comment'
+        fill_in "Comment", with: 'This is a comment'
+        click_button 'Leave Comment'
+        expect(current_path).to eq "/posts/#{first.id}"
+        expect(page).to have_content 'This is a comment' 
+      end
+
     end
 
   end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -2,64 +2,86 @@ require 'rails_helper'
 
 describe 'posts page' do
 
-  context 'when no posts have been added' do
+  context 'when not signed in' do
 
-    it 'should ask you to add a post' do
-      visit '/'
-      expect(page).to have_content 'We don\'t have any posts yet!'
-      expect(page).to have_link 'Why not add the first?'
-    end
-
-    it 'should let you create a post' do
+    it 'should not allow posts' do
       visit '/'
       click_link 'Why not add the first?'
-      fill_in 'Name', with: 'New Post'
-      click_button 'Create Post'
-      expect(page).to have_content 'New Post'
-      expect(current_path).to eq '/'
+      expect(page).to have_content 'Sign up'
     end
-
 
   end
 
-  context 'when posts have been added' do
-    # before do
-    #   Post.create(name:'First')
-    # end
+  context 'when signed in' do
 
-    let!(:first){Post.create(name:'First')}
-
-    it 'should show the posts' do
+    before do
       visit '/'
-      expect(page).to have_content 'First'
-      expect(page).not_to have_content 'We don\'t have any posts yet!'
+      click_link('Register')
+      fill_in('Email', with: 'test@tester.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
     end
 
-    it 'should let you add a post' do
-      visit '/'
-      click_link 'Add a new post!'
-      fill_in 'Name', with: 'New Post'
-      click_button 'Create Post'
-      expect(page).to have_content 'New Post'
-      
-      expect(current_path).to eq '/'
+    context 'when no posts have been added' do
+
+      it 'should ask you to add a post' do
+        visit '/'
+        expect(page).to have_content 'We don\'t have any posts yet!'
+        expect(page).to have_link 'Why not add the first?'
+      end
+
+      it 'should let you create a post' do
+        visit '/'
+        click_link 'Why not add the first?'
+        fill_in 'Name', with: 'New Post'
+        click_button 'Create Post'
+        expect(page).to have_content 'New Post'
+        expect(current_path).to eq '/'
+      end
+
+
     end
 
-    it 'should give each post its own page' do
-      visit '/'
-      click_link 'First'
-      expect(page).to have_content 'First'
-      expect(current_path).to eq "/posts/#{first.id}"
-    end
+    context 'when posts have been added' do
+      # before do
+      #   Post.create(name:'First')
+      # end
 
-    it 'should let a post be deleted' do
-      visit '/'
-      click_link 'First'
-      click_link 'Delete Post'
-      expect(page).not_to have_content 'First'
-      expect(page).to have_content 'Post deleted'
-    end
+      let!(:first){Post.create(name:'First')}
 
+      it 'should show the posts' do
+        visit '/'
+        expect(page).to have_content 'First'
+        expect(page).not_to have_content 'We don\'t have any posts yet!'
+      end
+
+      it 'should let you add a post' do
+        visit '/'
+        click_link 'Add a new post!'
+        fill_in 'Name', with: 'New Post'
+        click_button 'Create Post'
+        expect(page).to have_content 'New Post'
+        
+        expect(current_path).to eq '/'
+      end
+
+      it 'should give each post its own page' do
+        visit '/'
+        click_link 'First'
+        expect(page).to have_content 'First'
+        expect(current_path).to eq "/posts/#{first.id}"
+      end
+
+      it 'should let a post be deleted' do
+        visit '/'
+        click_link 'First'
+        click_link 'Delete Post'
+        expect(page).not_to have_content 'First'
+        expect(page).to have_content 'Post deleted'
+      end
+
+    end
   end
 
 end
